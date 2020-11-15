08e6281375b70d2cc2d5fe436346e6f41292eeae
unknown <180113017@stu.sdu.edu.kz>
2020-11-16 02:36:32 +0600

    ADDED: Scratch for Redux

--- src/Components/Users/Table.tsx
+++ src/Components/Users/Table.tsx
@@ -1,125 +1,144 @@
 import { Button, Divider, Table } from "antd";
-import React from "react";
+import Column from "antd/lib/table/Column";
+import Loader from "react-loader-spinner";
+import React, { useEffect, useState } from "react";
 
 let style = {
   paddingBottom: "3.5vh",
 };
 
-const columns: any = [
-  {
-    title: <b>Full name</b>,
-    dataIndex: "name",
-    fixed: "left",
-    width: "25%",
-    render: (text: String) => <a href="#">{text}</a>
-  },
-  {
-    title: <b>ID</b>,
-    dataIndex: "id",
-    fixed: "left",
-    width: "20%",
-  },
-  {
-    title: <b>Date created</b>,
-    dataIndex: "date",
-  },
-  {
-    title: <b>Enrollments</b>,
-    dataIndex: 'enrollments',
-  },
-  {
-    title: <b>GPA</b>,
-    dataIndex: 'gpa',
-  },
-  {
-    title: 
-      <Button style={{ float: "right", marginRight: "1.25vw" }}>
-        Delete
-      </Button>,
-    dataIndex: '',
-    fixed: "right",
-  }
-];
-
 export interface record {
-  id: any,
-  key: any,
-  name: String,
-  date: any,
-  enrollments: any,
-  gpa: any,
+  id: any;
+  key: any;
+  name: String;
+  date: any;
+  enrollments: any;
+  gpa: any;
 }
 
-let data: record[] = [];
-for (var i = 0; i < 46; i++) {
-  data.push({
-    key: i,
-    id: (i < 10) ? "18011000"+i : "1801100" + i,
-    name: "Naruto Uzumaki " + i,
-    date: "Nov 15, 2020",
-    enrollments: Math.round(Math.random() * (8 - 1) + 1),
-    gpa: (Math.random() * (4 - 2) + 2).toFixed(2),
-  });
-}
-for (i = 100; i < 500; i++) {
-  data.push({
-    key: i,
-    id: "180110" + i,
-    name: "Naruto Uzumaki " + i,
-    date: "Nov 15, 2020",
-    enrollments: Math.round(Math.random() * (8 - 1) + 1),
-    gpa: (Math.random() * (4 - 2) + 2).toFixed(2),
-  });
-}
+const UserTable = () => {
+  const [selectedKeys, setSelectedKeys] = useState<any | []>([]);
+  const [selected, setSelected] = useState(0);
+  const [tableData, setTableData] = useState<any>([]);
+  const [loading, setLoading] = useState(false);
 
-class UserTable extends React.Component {
-  state = {
-    selectedRowKeys: [] as unknown as any,
-  };
-  selectRow = (record: record) => {
-    const selectedRowKeys = [this.state.selectedRowKeys];
+  useEffect(() => {
+    const data = [];
+    for (var i = 0; i < 46; i++) {
+      data.push({
+        key: i,
+        id: i < 10 ? "18011000" + i : "1801100" + i,
+        name: "Naruto Uzumaki " + i,
+        date: "Nov 15, 2020",
+        enrollments: Math.round(Math.random() * (8 - 1) + 1),
+        gpa: (Math.random() * (4 - 2) + 2).toFixed(2),
+      });
+    }
+    for (i = 100; i < 500; i++) {
+      data.push({
+        key: i,
+        id: "180110" + i,
+        name: "Naruto Uzumaki " + i,
+        date: "Nov 15, 2020",
+        enrollments: Math.round(Math.random() * (8 - 1) + 1),
+        gpa: (Math.random() * (4 - 2) + 2).toFixed(2),
+      });
+    }
+    setTableData(data);
+  }, []);
+
+  const selectRow = (record: record) => {
+    const selectedRowKeys = [selectedKeys];
     if (selectedRowKeys.indexOf(record.key) >= 0) {
       selectedRowKeys.splice(selectedRowKeys.indexOf(record.key));
-    }
-    else
-      selectedRowKeys.push(record.key);
-    this.setState({ selectedRowKeys });
-  }
-  onSelectedRowKeyChange = (selectedRowKeys: any[]) => {
-    this.setState({ selectedRowKeys });
-    console.log('selectedRowKeys changed: ', selectedRowKeys);
-  }
-  render() {
-    const { selectedRowKeys } = this.state;
-    const rowSelection = {
-      selectedRowKeys,
-      onChange: this.onSelectedRowKeyChange,
-    }
-    return (
-      <div className="UserTable">
-        <h1>Users</h1>
-        <Divider style={{ margin: "2.5vh 0" }} />
-        <Table
-          rowSelection={rowSelection}
-          size="small"
-          pagination={
-            {
+    } else selectedRowKeys.push(record.key);
+    setSelectedKeys({ selectedRowKeys });
+  };
+  const onSelectedRowKeyChange = (selectedRowKeys: any[]) => {
+    setSelectedKeys({ selectedRowKeys });
+    setSelected(selectedRowKeys.length);
+    console.log("selectedRowKeys changed: ", selectedRowKeys);
+  };
+
+  const { selectedRowKeys } = selectedKeys;
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectedRowKeyChange,
+  };
+
+  const deleteRows = () => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      const selectedRowKeys = [selectedKeys];
+      tableData.filter((el: record) => !selectedRowKeys.includes(el.key));
+      selectedRowKeys.filter((el) => selectedRowKeys.includes(el));
+      setSelectedKeys(selectedRowKeys);
+      setSelected(0);
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  };
+
+  return (
+    <div className="UserTable">
+      <h1>Users</h1>
+      <Divider style={{ margin: "2.5vh 0" }} />
+      {loading ? (
+        <Loader
+          type="Bars"
+          color="#00BFFF"
+          height={50}
+          width={50}
+          timeout={3000}
+        />
+      ) : (
+        <>
+          <span
+            style={{
+              position: "absolute",
+              transform: "translate(75%, 95%)",
+            }}
+          >
+            Selected rows: {selected}
+          </span>
+          <Table
+            rowSelection={rowSelection}
+            pagination={{
               position: ["topRight", "topRight"],
               pageSize: 25,
-            }
-          }
-          columns={columns}
-          dataSource={data}
-          onRow={(record: any) => ({
-            onClick: () => {
-              this.selectRow(record);
-            },
-          })}
-          style={style}
-        />
-      </div>
-    );
-  }
-}
+            }}
+            dataSource={tableData}
+            style={style}
+          >
+            <Column
+              title={<b>Full name</b>}
+              dataIndex="name"
+              fixed="left"
+              width="25%"
+              render={(text: any) => <a href="/#">{text}</a>}
+            />
+            <Column title={<b>ID</b>} dataIndex="id" fixed="left" width="20%" />
+            <Column title={<b>Date created</b>} dataIndex="date" />
+            <Column title={<b>Enrollments</b>} dataIndex="enrollments" />
+            <Column title={<b>GPA</b>} dataIndex="gpa" />
+            <Column
+              title={
+                <Button
+                  disabled={selected === 0}
+                  style={{ float: "right", marginRight: "1.25vw" }}
+                  onClick={deleteRows}
+                >
+                  Delete ({selected})
+                </Button>
+              }
+              dataIndex=""
+              fixed="right"
+            />
+          </Table>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default UserTable;
