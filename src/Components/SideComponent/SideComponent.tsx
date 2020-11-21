2d211ff9d608b5828d4c575ecc5bbfc49905a592
unknown <180113017@stu.sdu.edu.kz>
2020-11-21 23:08:32 +0600

    ADDED: Side panel layout is finished

--- src/Components/SideComponent/SideComponent.tsx
+++ src/Components/SideComponent/SideComponent.tsx
@@ -1,8 +1,11 @@
 import { Collapse, Divider } from "antd";
 import React from "react";
 import {
+  ExportOutlined,
+  FrownOutlined,
+  HomeFilled,
+  PieChartOutlined,
   SettingOutlined,
-  EditOutlined,
   UsergroupDeleteOutlined,
 } from "@ant-design/icons";
 import { Link } from "react-router-dom";
@@ -13,10 +16,24 @@ const iconStyle = {
   marginRight: "5px",
 };
 
+const supportStudents = (<p style={{color: "white", marginLeft: "0.5vw", marginBottom: "0",}}>SUPPORT YOUR STUDENTS</p>);
+
+const pfp = "https://gamespot1.cbsistatic.com/uploads/scale_landscape/1352/13527689/2744541-gg_xrd_sign_ramlethal_v_ky_ps4_112514.jpg";
 const SideComponent = () => {
   const labels = [
     {
-      label: "SUPPORT YOUR STUDENTS",
+      label: "HOMEPAGE",
+      icon: <HomeFilled style={iconStyle} />,
+      divided: false,
+      link: "/",
+    },
+    {
+      label: "ADVANCED REPORTING",
+      icon: <PieChartOutlined style={iconStyle} />,
+      divided: false,
+    },
+    {
+      label: supportStudents,
       icon: <UsergroupDeleteOutlined style={iconStyle} />,
       divided: true,
       options: [
@@ -37,8 +54,18 @@ const SideComponent = () => {
     {
       label: "SETTINGS",
       icon: <SettingOutlined style={iconStyle} />,
+      divided: true,
+    },
+    {
+      label: "MY ACCOUNT",
+      icon: <FrownOutlined style={iconStyle} />,
       divided: false,
     },
+    {
+      label: "LOG OUT",
+      icon: <ExportOutlined style={iconStyle} />,
+      divided: false,
+    }
   ];
   return (
     <div
@@ -49,33 +76,59 @@ const SideComponent = () => {
     >
       {labels.map((e, index) => (
         <>
-          <Collapse
-            ghost
-            style={{ marginTop: "1.5vh" }}
-            defaultActiveKey={["0"]}
-            expandIcon={({ isActive }) => e.icon}
-          >
-            <Panel header={e.label} key={index}>
-              <ul className="option-content">
-                {e.options?.map((e) => (
-                  <li>
-                    <Link to={`/${e.link}`}>
-                      <a>{e.label}</a>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </Panel>
-          </Collapse>
-          { e.divided ?
-            <div style={{
-              width: "90%",
-              margin: "0 auto",
-            }}>
-              <Divider style={{ border: "1px solid white", }} /> 
+          {e.label !== supportStudents ? (
+            <div className="side-panel">
+              <div className="side-panel-item">
+                {e.icon}
+              <a style={{marginLeft: "0.5vw", transform: "translateY(-5%)",
+                color: e.label === "LOG OUT" ? "red" : "white", 
+              }}>
+                {e.label}
+                </a>
+              </div>
             </div>
-             : null
-          }
+          ) : (
+            <Collapse
+              ghost
+              style={{ marginTop: "1.5vh" }}
+              defaultActiveKey={["2"]}
+              expandIcon={({ isActive }) =>
+                e.label === "SETTINGS" ? (
+                  <SettingOutlined
+                    style={iconStyle}
+                    rotate={isActive ? 90 : 0}
+                  />
+                ) : (
+                  e.icon
+                )
+              }
+            >
+              <Panel header={e.label} key={index}>
+                <ul className="option-content">
+                  {e.options?.map((e) => (
+                    <li>
+                      <Link to={`/${e.link}`}>
+                        <a>{e.label}</a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </Panel>
+            </Collapse>
+          )}
+          {e.divided ? (
+            <div
+              style={{
+                width: "90%",
+                margin: "0 auto",
+              }}
+            >
+              <Divider style={{
+                 border: "0.1em solid rgba(245, 245, 245, 0.3)",
+                 margin: "2.5vh 0 1.5vh 0",
+              }} />
+            </div>
+          ) : null}
         </>
       ))}
     </div>
